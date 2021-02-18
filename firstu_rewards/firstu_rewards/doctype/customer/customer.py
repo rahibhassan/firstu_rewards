5c962ac6f5e4974d54db0d66e21276d3631927c6
Raheeb <raheeb@tridz.com>
2021-02-18 18:16:12 +0530

    User creation using random during signup

--- firstu_rewards/firstu_rewards/doctype/customer/customer.py
+++ firstu_rewards/firstu_rewards/doctype/customer/customer.py
@@ -7,16 +7,23 @@ import frappe
 from frappe.model.document import Document
 from frappe.core.doctype.user.user import generate_keys
 import random
+import string
 
 class Customer(Document):
 	def before_save(self):
-		rand_no = random.randrange(100, 999, 1)
-		email = self.customer.lower()+ "_" + self.last_name.lower() + str(rand_no) + "@" + "gmail.com"
 		if not self.user_id:
+			rand_no = random.randrange(10000, 99999, 1)
+			rand_string = ''.join(random.choices(string.ascii_lowercase, k = 10))
+			service_provider = ['gmail', 'ymail', 'yahoo', 'hotmail']
+			provider = random.choices(service_provider)[0]
+			rand_email = rand_string + str(rand_no) + '@' + provider + '.com'		
+			email = rand_email
+		
+		
 			user_doc = frappe.get_doc({
 				'doctype': 'User',
 				'email': email,
-				'first_name': self.customer,
+				'first_name': rand_string,
 				'send_welcome_email': 0,
 				'role_profile_name': 'FirstU Customer'
 				})
